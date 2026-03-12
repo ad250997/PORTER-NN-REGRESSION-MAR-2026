0d037b9d993b0cc747310285a4ab6c282046c9c5
Atharva Deshpande <atharvadeshpande925@gmail.com>
2026-03-12 11:51:19 +0530

    Delete run.ps1

--- run.ps1
+++
@@ -1,7 +0,0 @@
-Start-Process powershell -ArgumentList "-NoExit", "-Command", "py app.py"
-
-Start-Sleep -Seconds 5
-
-Start-Process powershell -ArgumentList "-NoExit", "-Command", "py -m streamlit run streamlit_app.py"
-
-Start-Sleep -Seconds 5
